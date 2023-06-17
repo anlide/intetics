1c4090d1adc0ef0eb40b3c040e3173832ec056c3
Alexander Baranov <elfanlide@gmail.com>
2023-06-17 19:06:53 +0200

    Set input size limit

--- Controller.php
+++ Controller.php
@@ -5,7 +5,8 @@ namespace Controller;
 use Sender\Sender;
 
 class Textarea {
-    private const placeholder = 'Enter any text';
+    private const PLACEHOLDER = 'Enter any text';
+    private const STRING_LIMIT = 64000;
 
     /**
      * @var \Model\Textarea|null
@@ -20,7 +21,7 @@ class Textarea {
         // TODO: Use model here for 'value'
         $this->textarea = new \Model\Textarea();
         return [
-            'placeholder' => self::placeholder,
+            'placeholder' => self::PLACEHOLDER,
             'value' => $this->textarea->getValue(),
             'error' => '',
         ];
@@ -36,26 +37,26 @@ class Textarea {
         $this->textarea = new \Model\Textarea();
         // TODO: escape input data
         $data = $_POST['textarea'];
-        $this->textarea->setValue($data);
-        // TODO: Save data
-        $errorText = '';
         try {
+            $this->textarea->setValue($data);
+            if (strlen($data) > self::STRING_LIMIT) {
+                throw new \Exception('String length should be less then '.self::STRING_LIMIT.' symbols');
+            }
             $autoIncrementValue = $this->textarea->insertToDatabase();
             $errorText = 'Inserted: '.$autoIncrementValue;
+            try {
+                $senderType = (rand(0, 100) > 50) ? Sender::SENDER_TYPE_EMAIL : Sender::SENDER_TYPE_SMS;
+                $sender = Sender::getSender($senderType);
+                $sender->send($data);
+                $errorText .= ' | Sent via '.$sender->getType();
+            } catch (\Exception $exception) {
+                $errorText .= ' | Not sent: '.$exception->getMessage();
+            }
         } catch (\Exception $exception) {
             $errorText = 'Not saved: '.$exception->getMessage();
         }
-        try {
-
-            $senderType = (rand(0, 100) > 50) ? Sender::SENDER_TYPE_EMAIL : Sender::SENDER_TYPE_SMS;
-            $sender = Sender::getSender($senderType);
-            $sender->send($data);
-            $errorText .= ' | Sent via '.$sender->getType();
-        } catch (\Exception $exception) {
-            $errorText .= ' | Not sent: '.$exception->getMessage();
-        }
         return [
-            'placeholder' => self::placeholder,
+            'placeholder' => self::PLACEHOLDER,
             'value' => $this->textarea->getValue(),
             'error' => $errorText,
         ];
